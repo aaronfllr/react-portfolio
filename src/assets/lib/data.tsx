144792ebcfe5a2f4caf92d8d055fc5b0e00b68fc
aaronfllr <aaronfllr@gmail.com>
2024-09-02 10:57:56 -0400

    first commit

---
+++ src/assets/lib/data.tsx
@@ -0,0 +1,484 @@
+import strapiicon from "../../assets/icons/strapiicon.svg";
+import figmaicon from "../../assets/icons/figmaicon.svg";
+import htmlicon from "../../assets/icons/htmlicon.svg";
+import cssicon from "../../assets/icons/cssicon.svg";
+import javascripticon from "../../assets/icons/javascripticon.svg";
+import typescripticon from "../../assets/icons/typescripticon.svg";
+import reacticon from "../../assets/icons/reacticon.svg";
+import nextjsicon from "../../assets/icons/nextjsicon.svg";
+import nodejsicon from "../../assets/icons/nodejsicon.svg";
+import tailwindcssicon from "../../assets/icons/tailwindicon.svg";
+import sassscssicon from "../../assets/icons/sassicon.svg";
+import stripeicon from "../../assets/icons/stripeicon.svg";
+import netlifyicon from "../../assets/icons/netlifyicon.svg";
+import shopifyicon from "../../assets/icons/shopifyicon.svg";
+import webflowicon from "../../assets/icons/webflowicon.svg";
+import artisanPastry from "../../assets/img/artisanPastry.png";
+import carolinaSpa from "../../assets/img/carolinaSpa.png";
+import profilepicture from "../img/me2.png";
+import caricon from "../../assets/icons/car-icon.svg";
+import travelicon from "../../assets/icons/travel-icon.svg";
+import hardwareicon from "../../assets/icons/hardware-icon.svg";
+import nextjsiconwhite from "../../assets/icons/nextjsiconwhite.svg";
+import { GoHome, GoPerson, GoMail, GoStack, GoProject } from "react-icons/go";
+import { FiGithub, FiLink, FiLinkedin, FiMail } from "react-icons/fi";
+import Imprint from "../../components/Imprint";
+import Privacy from "../../components/Privacy";
+
+export const headerIntroData = {
+  title: {
+    en: "Hi, I'm Aaron",
+  },
+  subtitle: "Fullstack Developer ",
+  description: {
+    en: "I'm Aaron, a fullstack developer with the goal of advancing my career and participating in inspiring projects. Here I present my work and my passion for web development. Let's solve some problems!",
+  },
+  buttons: [
+    {
+      name: "Contact",
+      label: {
+        en: "Contact me",
+      },
+      icon: FiMail,
+      color: "main-btn",
+    },
+    {
+      name: "Projects",
+      label: {
+        en: "My Projects",
+      },
+      icon: FiGithub,
+      color: "secondary-btn",
+    },
+  ],
+  profilepicture: profilepicture,
+} as const;
+
+export const projectsData = [
+  {
+    title: "Artisan Pastry NC",
+    description_EN:
+      "A bakery storefront app that we built as a team with Nextjs and Supabase. I was the backend developer and programmed features like Stripe and Auth. The website is responsive and mobile-optimized.",
+    technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "Typescript", icon: typescripticon },
+      { name: "Nextjs", icon: nextjsicon },
+      { name: "Netlify", icon: netlifyicon },
+      { name: "Stripe", icon: stripeicon },
+    ],
+    image: artisanPastry,
+    deploymenturl: "https://artisanpastrync.com/",
+    githuburl: "https://github.com/artisanpastrync/artisanpastrync.com",
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#70B9BE",
+    },
+  },
+  {
+    title: "Carolina Spa Movers",
+    description_EN:
+      "A website for a local business that needed an online presence.",
+    technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "TypeScript", icon: typescripticon },
+      { name: "Tailwind", icon: tailwindcssicon },
+      { name: "Next.js", icon: nextjsicon },
+      { name: "Figma", icon: figmaicon },
+      { name: "Netlify", icon: netlifyicon },
+    ],
+    image: carolinaSpa,
+    deploymenturl: "https://main--carolina-spa-movers.netlify.app/",
+    githuburl:
+      "https://github.com/aaronfllr-freelance/carolina-spa-movers",
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#FFD5BD",
+    },
+  },
+] as const;
+
+export const liveTickerData = {
+  content: {
+    en: "More Projects on Github",
+  },
+} as const;
+
+export const skillsDataWeb = [
+  {
+    skillsTitle: "WebDevelopment",
+    skills: [
+      {
+        title: "HTML",
+        hash: "#html",
+        icon: htmlicon,
+        color: "#F1662A",
+      },
+      {
+        title: "CSS",
+        hash: "#CSS",
+        icon: cssicon,
+        color: "#1572B6",
+      },
+      {
+        title: "JavaScript",
+        hash: "#JavaScript",
+        icon: javascripticon,
+        color: "#F7DF1E",
+      },
+      {
+        title: "TypeScript",
+        hash: "#TypeScript",
+        icon: typescripticon,
+        color: "#007ACC",
+      },
+      {
+        title: "React",
+        hash: "#React",
+        icon: reacticon,
+        color: "#61DAFB",
+      },
+      {
+        title: "Next.js",
+        hash: "#Next.js",
+        icon: [nextjsicon, nextjsiconwhite],
+        color: ["#000000", "#FFFFFF"],
+      },
+      {
+        title: "Node.js",
+        hash: "#Node.js",
+        icon: nodejsicon,
+        color: "#339933",
+      },
+      {
+        title: "Tailwind",
+        hash: "#Tailwind",
+        icon: tailwindcssicon,
+        color: "#38B2AC",
+      },
+      {
+        title: "SASS/SCSS",
+        hash: "#SASS/SCSS",
+        icon: sassscssicon,
+        color: "#CC6699",
+      },
+    ],
+  },
+] as const;
+
+export const skillsDataDesign = [
+  {
+    skillsTitle: "Design",
+    skills: [
+      { title: "Figma", hash: "#Figma", icon: figmaicon, color: "#F24E1E" },
+    ],
+  },
+] as const;
+
+export const skillsDataCMS = [
+  {
+    skillsTitle: "CMS",
+    skills: [
+      {
+        title: "Strapi",
+        hash: "#Strapi",
+        icon: strapiicon,
+        color: "#21759B",
+      },
+    ],
+  },
+] as const;
+
+export const navLinks = [
+  {en: "Home", hash: "#home", icon: GoHome },
+  {en: "Skills", hash: "#skills", icon: GoStack },
+  {en: "Projects", hash: "#projects", icon: GoProject },
+  {en: "About me", hash: "#about-me", icon: GoPerson },
+  {en: "Contact", hash: "#contact", icon: GoMail },
+] as const;
+
+export const FooterLinks = [
+  {en: "Imprint", hash: "#imprint", data: <Imprint /> },
+  {en: "Privacy", hash: "#privacy", data: <Privacy /> },
+] as const;
+
+export const sideBarRightMail = {
+  link: "mailto:aaronfllr.work@gmail.com",
+  text: "aaronfllr.work@gmail.com",
+} as const;
+
+export const sideBarLeftSocials = [
+  {
+    link: "https://www.linkedin.com/in/aaron-fuller-32776910b/",
+    icon: FiLinkedin,
+    altimgname: "linkedin",
+  },
+  {
+    link: "https://github.com/aaronfllr",
+    icon: FiGithub,
+    altimgname: "github",
+  },
+  {
+    link: "mailto:aaronfllr.work@gmail.com",
+    icon: FiMail,
+    altimgname: "mail",
+  },
+] as const;
+
+export const quotesData = [
+  {
+    en: `"All our dreams can come true, if we have the courage to pursue them."`,
+    author: "Walt Disney",
+  },
+  {
+    en: `"The advance of technology is based on making it fit in so that you don't really even notice it, so it's part of everyday life."`,
+  },
+] as const;
+
+export const aboutMeData = {
+  title_EN: "About me",
+  description_EN: "A few snippets about me",
+  paragraphs_EN: [
+    {
+      title: "The Curios World, Digital and Physical",
+      description:
+        "When I'm not exploring the digital world, I explore mountains, hiking trails, and rivers.",
+      icon: hardwareicon,
+    },
+    {
+      title: "On the Sounds of Life",
+      description:
+        "Besides coding, I like to pluck strings, whether it is guitar or attempting a bass line at a family jam session.",
+      icon: caricon,
+    },
+    {
+      title: "The Joy of Discovery as a Life Motto",
+      description:
+        "My journey as a web developer is only a part of my life path. I live by the motto that you never stay static, you either learn new skills or fall behind.",
+      icon: travelicon,
+    },
+  ],
+};
+
+export const contactData = {
+  title: {
+    en: "Contact",
+  },
+  description: {
+    en: "Write me a message and I will get back to you.",
+  },
+  inputfields: [
+    {
+      name: "name",
+      placeholder: {
+        en: "Your Name",
+      },
+      type: "text",
+      validation: {
+        en: "Please fill in your name",
+      },
+      pattern: "{2}",
+    },
+    {
+      name: "email",
+      placeholder: {
+        en: "Your E-Mail",
+      },
+      type: "email",
+      validation: {
+        en: "Please fill in your email",
+      },
+      pattern: "[@]{4}",
+    },
+    {
+      name: "subject",
+      placeholder: {
+        en: "Your Subject",
+      },
+      type: "text",
+      validation: {
+        en: "Please fill in your subject",
+      },
+      pattern: "{10}",
+    },
+  ],
+  textarea: {
+    placeholder: {
+      en: "Your Message",
+    },
+    name: "message",
+    rows: 10,
+    validation: {
+      en: "Please fill in your message",
+    },
+    pattern: "{10}",
+  },
+  button: {
+    value: {
+      en: "Send",
+    },
+  },
+  icon: FiMail,
+  iconcolor: "#FFFFFF",
+  colors: {
+    main: "main-btn",
+    second: "secondary-btn",
+    icon: "white",
+  },
+  privacyOptIn: {
+    checkbox: {
+      en: "I agree that Aaron may use my personal data (name and e-mail address) to contact me.",
+    },
+    description: {
+      en: "By submitting this request, you acknowledge that you have read the Private Policy",
+    },
+  },
+} as const;
+
+export const toastMessages = {
+  loadingProject: {
+    en: "The live demo will open shortly. Starting servers...",
+  },
+  successEmailSent: {
+    en: "Thank you for your email. I will get back to you as soon as possible",
+  },
+  failedEmailSent: {
+    en: "Unfortunately the sending of your email did not work. Please try again later",
+  },
+  failedValidationName: {
+    en: "Please fill in your name",
+  },
+} as const;
+
+export const buttonLabels = {
+  language: {
+    en: "EN",
+  },
+} as const;
+
+export const directionStyles: Record<string, React.CSSProperties> = {
+  "outer-right-to-inner-left": {
+    marginRight: "4rem",
+  },
+  "outer-left-to-inner-right": {
+    marginLeft: "4rem",
+    transform: "scaleX(-1)",
+  },
+  "inner-right-to-middle": {
+    width: "100%",
+    transform: "scaleY(1)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  "inner-left-to-middle": {
+    width: "100%",
+    transform: "scaleX(-1)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  "middle-to-inner-right": {
+    width: "100%",
+    transform: "scale(1,-1)",
+  },
+  "middle-to-inner-left": {
+    width: "100%",
+    transform: "scale(-1,-1)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  middle: {
+    width: "100%",
+    transform: "scaleX(-1)",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+};
+
+export const heightStyles: Record<string, { heights: [string, string] }> = {
+  small: {
+    heights: ["25rem", "15rem"],
+  },
+  middle: {
+    heights: ["35rem", "25rem"],
+  },
+  large: {
+    heights: ["45rem", "35rem"],
+  },
+  extraLarge: {
+    heights: ["55rem", "45rem"],
+  },
+};
+
+export const spaceStyles: Record<string, React.CSSProperties> = {
+  "outer-right-to-inner-left": {
+    marginTop: "-6rem",
+    width: "100%",
+  },
+  "outer-left-to-inner-right": {
+    marginTop: "-6rem",
+    width: "100%",
+  },
+  "inner-right-to-middle": {
+    marginTop: "-20rem",
+    width: "50%",
+    zIndex: "-10",
+  },
+  "inner-left-to-middle": {
+    marginTop: "-10rem",
+    width: "50%",
+    zIndex: "-10",
+  },
+  "middle-to-inner-right": {
+    width: "75%",
+  },
+  "middle-to-inner-left": {
+    marginTop: "-2.5rem",
+    width: "50%",
+  },
+  middle: {
+    marginTop: "-2.5rem",
+    width: "0%",
+    display: "none",
+  },
+};
+
+export const widthStyles: Record<string, { widths: [string, string] }> = {
+  "outer-right-to-inner-left": {
+    widths: ["74.45%", "74.45%"],
+  },
+  "outer-left-to-inner-right": {
+    widths: ["75%", "75%"],
+  },
+  "inner-right-to-middle": {
+    widths: ["50.1%", "49%"],
+  },
+  "inner-left-to-middle": {
+    widths: ["50.1%", "49%"],
+  },
+  "middle-to-inner-right": {
+    widths: ["33.4%", "50.03%"],
+  },
+  "middle-to-inner-left": {
+    widths: ["50.1%", "49%"],
+  },
+  middle: {
+    widths: ["0%", "0%"],
+  },
+};
